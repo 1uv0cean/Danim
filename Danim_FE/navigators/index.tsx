8e73bad7cdc2d9404bfeac411b4987a246bebfb2
songhwee1 <songhwee1@naver.com>
2021-07-29 20:40:57 +0900

    add editprofile

--- Danim_FE/navigators/index.tsx
+++ Danim_FE/navigators/index.tsx
@@ -5,6 +5,7 @@ import Main, {MainParams} from '../views/Main';
 import Register from '../views/Register';
 import RegisterWait from '../views/RegisterWait';
 import MyPage from '../views/Mypage';
+import EditProfile from '../views/EditProfile';
 
 export enum HomeScreens {
   Login = 'Login',
@@ -12,14 +13,16 @@ export enum HomeScreens {
   Register = 'Register',
   RegisterWait = 'RegisterWait',
   MyPage = "MyPage",
+  EditProfile = "EditProfile"
 }
 
 export type HomeStackParamList = {
   Login: undefined;
-  Main: undefined;
+  Main: MainParams;
   Register: undefined;
   RegisterWait: undefined;
   MyPage: undefined;
+  EditProfile: undefined;
 };
 
 const HomeStack = createStackNavigator<HomeStackParamList>();
@@ -29,8 +32,12 @@ const HomeStackNavigator: React.FunctionComponent = () => {
       <HomeStack.Screen name={HomeScreens.Login} component={Login} />
       <HomeStack.Screen name={HomeScreens.Main} component={Main} />
       <HomeStack.Screen name={HomeScreens.Register} component={Register} />
-      <HomeStack.Screen name={HomeScreens.RegisterWait} component={RegisterWait} />
-      <HomeStack.Screen name={HomeScreens.MyPage} component={MyPage} />
+      <HomeStack.Screen
+        name={HomeScreens.RegisterWait}
+        component={RegisterWait}
+      />
+       <HomeStack.Screen name={HomeScreens.MyPage} component={MyPage} />
+       <HomeStack.Screen name={HomeScreens.EditProfile} component={EditProfile} />
     </HomeStack.Navigator>
   );
 };
