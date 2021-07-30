7661ea0764a5c8d81b7c803f6ddaabd51fe33008
chaminjae <beingeskimo@gmail.com>
2021-07-30 09:51:18 +0900

    Apply SMS(SENS) APi

--- Danim_FE/server/routes/index.js
+++ Danim_FE/server/routes/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
+const dotenv = require('dotenv');
+const path = require('path');
+var request = require('request');
+
+//create signature2
+var CryptoJS = require('crypto-js');
+
+dotenv.config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === 'production' ? '.env' : '.env.dev',
+  ),
+});
 
 router.get('/api', (req, res) => {
   // console.log('detected');
@@ -48,6 +61,72 @@ router.post('/api/login', (req, res) => {
       }
     },
   );
+});
+
+router.post('/api/post/sms', (req, res) => {
+  const userPhone = req.body.userPhone;
+
+  var resultCode = 404;
+
+  const date = Date.now().toString();
+  const uri = process.env.SERVICE_ID; //서비스 ID
+  const secretKey = process.env.NCP_SECRET_KEY; // Secret Key
+  const accessKey = process.env.NCP_KEY; //Access Key
+  const method = 'POST';
+  const space = ' ';
+  const newLine = '\n';
+  const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
+  const url2 = `/sms/v2/services/${uri}/messages`;
+
+  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+
+  hmac.update(method);
+  hmac.update(space);
+  hmac.update(url2);
+  hmac.update(newLine);
+  hmac.update(date);
+  hmac.update(newLine);
+  hmac.update(accessKey);
+
+  const hash = hmac.finalize();
+  const signature = hash.toString(CryptoJS.enc.Base64);
+
+  request(
+    {
+      method: method,
+      json: true,
+      uri: url,
+      headers: {
+        'Contenc-type': 'application/json; charset=utf-8',
+        'x-ncp-iam-access-key': accessKey,
+        'x-ncp-apigw-timestamp': date,
+        'x-ncp-apigw-signature-v2': signature,
+      },
+      body: {
+        type: 'SMS',
+        countryCode: '82',
+        from: process.env.PHONE_NUM,
+        content: `WEIVER 인증번호 ${userPhone} ${process.env.PHONE_NUM} 입니다.`,
+        messages: [
+          {
+            to: `${userPhone}`,
+          },
+        ],
+      },
+    },
+    function (err, res, html) {
+      if (err) {
+        console.log(err);
+      } else {
+        resultCode = 200;
+        console.log(html);
+      }
+    },
+  );
+
+  res.json({
+    code: resultCode,
+  });
 });
 
 module.exports = router;
