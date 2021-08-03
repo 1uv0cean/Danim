be76ebf4f3bd54c61a10a29443d3d02975b56796
chaminjae <beingeskimo@gmail.com>
2021-08-03 11:15:04 +0900

    Update change user phone number

--- Danim_FE/server/app.js
+++ Danim_FE/server/app.js
@@ -16,6 +16,7 @@ app.post('/api/login', api);
 app.post('/api/post/sms', api);
 app.post('/api/chkDuplicate', api);
 app.post('/api/busstop', api);
+app.post('/api/post/changePhone', api);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
