ab715478ac06e27e2889d2ace569c3f99f1d024a
songhwee1 <songhwee1@naver.com>
2021-08-05 11:32:36 +0900

    re-import

--- Danim_FE/views/EditProfile.tsx
+++ Danim_FE/views/EditProfile.tsx
@@ -3,10 +3,10 @@ import {HomeScreens, HomeStackParamList} from '../navigators/index';
 import {Text, View, StyleSheet, Alert} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {Button} from 'react-native-paper';
-import PhoneNumInput from '../components/editProfile/PhoneNumInput';
-import NumInput from '../components/editProfile/NumInput';
 import {funcPostSMS} from '../function/funcSendChangeSMS';
 import {funcChangePhone} from '../function/funcChangePhone';
+import PhoneNumInput from '../components/editprofile/PhoneNumInput';
+import NumInput from '../components/editprofile/NumInput';
 
 type EditProfileScreenNavigationProps = StackNavigationProp<
   HomeStackParamList,
