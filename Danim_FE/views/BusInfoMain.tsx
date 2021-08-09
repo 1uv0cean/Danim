9bd63c8542596319f668331187f1736566edb6c4
songhwee1 <songhwee1@naver.com>
2021-08-09 11:18:42 +0900

    add reservation button

--- Danim_FE/views/BusInfoMain.tsx
+++ Danim_FE/views/BusInfoMain.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-paper';
 import {createAppContainer} from 'react-navigation';
 import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
 import Info1 from './BusInfo1';
 import Info2 from './BusInfo2';
+
+const styles = StyleSheet.create({
+  RegisterButton: {
+    color: 'white',
+    height: 50,
+    backgroundColor: '#2C3E50',
+    marginTop:20,
+    margin: 10,
+    padding: 4
+  },
+  Text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+})
 
 const topNavitgator = createMaterialTopTabNavigator(
   {
@@ -31,6 +48,9 @@ const busInfoMain = () => {
     return( 
       <>
         <AppContainer></AppContainer>
+        <Button style={styles.RegisterButton}>
+          <Text style={styles.Text}>예약</Text>
+        </Button>
       </>
     );
 };
